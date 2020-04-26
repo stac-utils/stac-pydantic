290f122dcf096307d72a54ce1519ccc52fe33271
Jeff Albrecht <geospatialjeff@gmail.com>
2020-04-26 12:21:06 -0500

    update label extension

--- stac_pydantic/extensions/label.py
+++ stac_pydantic/extensions/label.py
@@ -14,44 +14,44 @@ class LabelTypes(str, AutoValueEnum):
 
 class ClassObject(BaseModel):
     """
-    https://github.com/radiantearth/stac-spec/blob/v0.0.9/extensions/label#class-object
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/label#class-object
     """
 
-    name: Union[str, None]
-    classes: List[Union[str, NumType]]
+    name: Optional[Union[str]]
+    classes: Optional[List[Union[str, NumType]]]
 
 
 class CountObject(BaseModel):
     """
-    https://github.com/radiantearth/stac-spec/blob/v0.0.9/extensions/label#count-object
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/label#count-object
     """
 
-    name: str
-    count: int
+    name: Optional[str]
+    count: Optional[int]
 
 
 class StatsObject(BaseModel):
     """
-    https://github.com/radiantearth/stac-spec/blob/v0.0.9/extensions/label#stats-object
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/label#stats-object
     """
 
-    name: str
-    value: NumType
+    name: Optional[str]
+    value: Optional[NumType]
 
 
 class OverviewObject(BaseModel):
     """
-    https://github.com/radiantearth/stac-spec/blob/v0.0.9/extensions/label#label-overview-object
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/label#label-overview-object
     """
 
-    property_key: str
+    property_key: Optional[str]
     counts: Optional[List[CountObject]]
     statistics: Optional[List[StatsObject]]
 
 
 class LabelExtension(BaseModel):
     """
-    https://github.com/radiantearth/stac-spec/blob/v0.0.9/extensions/label#label-extension-specification
+    https://github.com/radiantearth/stac-spec/tree/v0.9.0/extensions/label#label-extension-specification
     """
 
     properties: List[Union[str, None]]
