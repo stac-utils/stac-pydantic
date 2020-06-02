48244fe50041b78551b6f40c0fae21087c8426e7
Jeff Albrecht <geospatialjeff@gmail.com>
2020-06-01 20:21:25 -0500

    validate datetime

--- stac_pydantic/api/search.py
+++ stac_pydantic/api/search.py
@@ -2,12 +2,12 @@ from datetime import datetime
 from typing import Any, Dict, List, Optional, Union
 
 from geojson_pydantic.geometries import Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon
-from pydantic import BaseModel, Field, root_validator
+from pydantic import BaseModel, Field, validator
 
 from .extensions.fields import FieldsExtension
 from .extensions.query import Operator
 from .extensions.sort import SortExtension
-from ..shared import BBox
+from ..shared import BBox, DATETIME_RFC339
 
 
 class Search(BaseModel):
@@ -15,15 +15,39 @@ class Search(BaseModel):
     ids: Optional[List[str]]
     bbox: Optional[BBox]
     intersects: Optional[Union[Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon]]
-    datetime: Optional[Union[str, datetime]]
+    datetime: Optional[str]
     limit: int = 10
     field: Optional[FieldsExtension] = Field(None, alias="fields")
     query: Optional[Dict[str, Dict[Operator, Any]]]
     sortby: Optional[List[SortExtension]]
 
-    @root_validator
-    def validate_spatial_query(cls, values):
-        # Validate geometry params
-        if values['intersects'] and values['bbox']:
+    @validator("intersects")
+    def validate_spatial(cls, v, values):
+        if v and values['bbox']:
             raise ValueError("intersects and bbox parameters are mutually exclusive")
         return values
+
+    @validator("datetime")
+    def validate_datetime(cls, v):
+        if '/' in v:
+            values = v.split('/')
+        else:
+            # Single date is interpreted as end date
+            values = ["..", v]
+
+        dates = []
+        for value in values:
+            if value == "..":
+                dates.append(value)
+                continue
+            try:
+                datetime.strptime(value, DATETIME_RFC339)
+                dates.append(value)
+            except:
+                raise ValueError(f"Invalid datetime, must match format ({DATETIME_RFC339}).")
+
+        if ".." not in dates:
+            if datetime.strptime(dates[0], DATETIME_RFC339) > datetime.strptime(dates[1], DATETIME_RFC339):
+                raise ValueError(f"Invalid datetime range, must match format (begin_date, end_date).")
+
+        return dates
