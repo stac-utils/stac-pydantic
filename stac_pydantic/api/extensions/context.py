ddd62ed0397cea8c67f11404ec55670fc6e236d1
Jeff Albrecht <geospatialjeff@gmail.com>
2020-06-01 20:56:09 -0500

    validate context extension returned

--- stac_pydantic/api/extensions/context.py
+++ stac_pydantic/api/extensions/context.py
@@ -1,9 +1,14 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class ContextExtension(BaseModel):
     returned: int
     limit: Optional[int]
     matched: Optional[int]
+
+    @validator("limit")
+    def validate_limit(cls, v, values):
+        if values['returned'] > v:
+            raise ValueError("Number of returned items must be less than or equal to the limit")
