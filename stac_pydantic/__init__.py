d9b66f54f0b4fc9dc30ec0e2067ee5237448419e
Jeff Albrecht <geospatialjeff@gmail.com>
2020-08-10 15:37:09 -0500

    add validate_item convenience wrapper

--- stac_pydantic/__init__.py
+++ stac_pydantic/__init__.py
@@ -1,4 +1,10 @@
 from .catalog import Catalog
 from .collection import Collection
 from .extensions import Extensions
-from .item import Item, ItemCollection, ItemProperties, item_model_factory
+from .item import (
+    Item,
+    ItemCollection,
+    ItemProperties,
+    item_model_factory,
+    validate_item,
+)
