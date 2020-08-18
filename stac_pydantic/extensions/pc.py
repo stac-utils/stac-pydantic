421741d9754797bb431d3af480eb0ece49fa1b0e
Jeff Albrecht <geospatialjeff@gmail.com>
2020-08-18 10:10:12 -0500

    add test, cleanup

--- stac_pydantic/extensions/pc.py
+++ stac_pydantic/extensions/pc.py
@@ -60,4 +60,3 @@ class PointCloudExtension(BaseModel):
     class Config:
         use_enum_values = True
         allow_population_by_field_name = True
-        # alias_generator = lambda field_name: f"pc:{field_name}"
