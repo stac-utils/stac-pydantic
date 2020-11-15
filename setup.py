8e20f5396cc5c50efffc3d12036476820d86a573
Jeff Albrecht <geospatialjeff@gmail.com>
2020-11-15 11:30:31 -0600

    release-prep (#36)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Pydantic data models for the STAC spec",
     long_description=desc,
     long_description_content_type="text/markdown",
-    version="1.3.6",
+    version="1.3.7",
     python_requires=">=3.6",
     classifiers=[
         "Intended Audience :: Developers",
