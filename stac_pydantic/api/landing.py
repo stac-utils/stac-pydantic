e4a3f330abd5d15102c2b0090a83cbf6e3b63ef1
Rob Emanuele <rdemanuele@gmail.com>
2020-11-30 19:50:09 -0500

    Add 'id' to LandPage to make it a valid Catalog. (#43)

--- stac_pydantic/api/landing.py
+++ stac_pydantic/api/landing.py
@@ -11,6 +11,7 @@ class LandingPage(BaseModel):
     https://github.com/radiantearth/stac-api-spec/blob/master/api-spec.md#ogc-api---features-endpoints
     """
 
+    id: str
     description: str
     title: Optional[str]
     stac_version: str = Field(STAC_VERSION, const=True)
