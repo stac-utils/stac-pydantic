ac73b1e586d4a3ba3c1ca845cd4922d250599435
Jeff Albrecht <geospatialjeff@gmail.com>
2021-02-21 18:43:21 -0600

    add dataclasses backport for python3.6

--- setup.py
+++ setup.py
@@ -30,7 +30,12 @@ setup(
     license="MIT",
     packages=find_packages(exclude=["tests"]),
     zip_safe=False,
-    install_requires=["click", "pydantic>=1.6", "geojson-pydantic",],
+    install_requires=[
+        "click",
+        "pydantic>=1.6",
+        "geojson-pydantic",
+        "dataclasses; python_version <= '3.6'",
+    ],
     tests_require=["pytest", "pytest-cov", "requests", "shapely"],
     setup_requires=["pytest-runner"],
     entry_points={"console_scripts": ["stac-pydantic=stac_pydantic.scripts.cli:app"]},
