1a514ee13ab24e3734bf2d71ee7ebc5dae29e343
Jeff Albrecht <geospatialjeff@gmail.com>
2021-02-22 20:06:26 -0600

    make sure dates match

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
+import operator
+from datetime import datetime, timedelta
+
+import arrow
 import dictdiffer
 import pytest
 import requests
 from click.testing import CliRunner
+
+from stac_pydantic.shared import DATETIME_RFC339
 
 
 def request(url: str):
@@ -19,6 +25,15 @@ def dict_match(d1: dict, d2: dict):
         # same for bbox
         elif "bbox" in diff[1]:
             assert list(diff[2][0]) == list(diff[2][1])
+        # test data is pretty variable with how it represents datetime, RFC3339 is quite flexible
+        # but stac-pydantic only supports a single datetime format, so just validate to the day.
+        elif "datetime" in diff[1]:
+            dates = []
+            for date in diff[2]:
+                if isinstance(date, str):
+                    date = arrow.get(date)
+                dates.append(date)
+            assert operator.sub(*dates).days == 0
         # any other differences are errors
         else:
             raise AssertionError("Unexpected difference: ", diff)
