b6332605337c2ab88dbcea92e5defdba2d537e83
Jeff Albrecht <geospatialjeff@gmail.com>
2021-03-11 21:46:31 -0600

    use a mapping to define operations

--- stac_pydantic/api/extensions/query.py
+++ stac_pydantic/api/extensions/query.py
@@ -1,4 +1,3 @@
-import operator
 from enum import auto
 from types import DynamicClassAttribute
 from typing import Any, Callable
@@ -7,6 +6,18 @@ from stac_pydantic.utils import AutoValueEnum
 
 # TODO: These are defined in the spec but aren't currently implemented by the operator syntax
 UNSUPPORTED_OPERATORS = {"startsWith", "endsWith", "contains", "in"}
+
+_OPERATIONS = {
+    "eq": lambda x, y: x == y,
+    "ne": lambda x, y: x != y,
+    "lt": lambda x, y: x < y,
+    "le": lambda x, y: x <= y,
+    "gt": lambda x, y: x > y,
+    "ge": lambda x, y: x > y,
+    "startsWith": lambda x, y: x.startsWith(y),
+    "endsWith": lambda x, y: x.endsWith(y),
+    "constains": lambda x, y: y in x,
+}
 
 
 class Operator(str, AutoValueEnum):
@@ -27,6 +38,4 @@ class Operator(str, AutoValueEnum):
     @DynamicClassAttribute
     def operator(self) -> Callable[[Any, Any], bool]:
         """Return python operator"""
-        if self._value_ in UNSUPPORTED_OPERATORS:
-            raise NotImplementedError
-        return getattr(operator, self._value_)
+        return _OPERATIONS[self._value_]
