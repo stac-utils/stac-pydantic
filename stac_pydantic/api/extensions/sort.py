ad0602e1020605ce5b2aafaf01bcffe37f4ad7d3
Nathan Zimmerman <npzimmerman@gmail.com>
2021-06-09 21:32:42 -0500

    const -> constr

--- stac_pydantic/api/extensions/sort.py
+++ stac_pydantic/api/extensions/sort.py
@@ -1,6 +1,6 @@
 from enum import auto
 
-from pydantic import BaseModel
+from pydantic import constr, BaseModel
 
 from stac_pydantic.utils import AutoValueEnum
 
@@ -15,5 +15,5 @@ class SortExtension(BaseModel):
     https://github.com/radiantearth/stac-api-spec/tree/master/extensions/sort#sort-api-extension
     """
 
-    field: const(min_length=1)
+    field: constr(min_length=1)
     direction: SortDirections
