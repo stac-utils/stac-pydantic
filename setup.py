b712c33e1db42b1dc392d4995ca19eca8902ba65
Rob Emanuele <rdemanuele@gmail.com>
2021-06-28 19:35:16 -0400

    Update version to 2.0.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description="Pydantic data models for the STAC spec",
     long_description=desc,
     long_description_content_type="text/markdown",
-    version="1.3.9",
+    version="2.0.0",
     python_requires=">=3.7",
     classifiers=[
         "Intended Audience :: Developers",
