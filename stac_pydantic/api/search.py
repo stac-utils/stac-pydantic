00eece9c612f9c74262b65d944a5dfabc2103437
Nathan Zimmerman <npzimmerman@gmail.com>
2021-11-22 21:39:39 -0600

    Add extended search

--- stac_pydantic/api/search.py
+++ stac_pydantic/api/search.py
@@ -22,6 +22,8 @@ from stac_pydantic.shared import BBox
 
 class Search(BaseModel):
     """
+    The base class for STAC API searches.
+
     https://github.com/radiantearth/stac-api-spec/blob/master/api-spec.md#filter-parameters-and-fields
     """
 
@@ -141,3 +143,13 @@ class Search(BaseModel):
         if self.intersects:
             return self.intersects
         return
+
+
+class ExtendedSearch(Search):
+    """
+    STAC API search with extensions enabled.
+    """
+
+    field: Optional[FieldsExtension] = Field(None, alias="fields")
+    query: Optional[Dict[str, Dict[Operator, Any]]]
+    sortby: Optional[List[SortExtension]]
