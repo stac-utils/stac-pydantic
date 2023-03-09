5df1f9976e790b72ba972a65d1b08eb2cc149e20
Phil Varner <pvarner@element84.com>
2023-03-09 14:46:04 -0500

    Fix STAC API Query Extension operator names from ne->neq, le->lte, and ge->gte, fix operations mappings

--- tests/test_models.py
+++ tests/test_models.py
@@ -196,6 +196,7 @@ def test_api_conformance_invalid_url():
 
 def test_api_landing_page():
     LandingPage(
+        type="Catalog",
         id="test-landing-page",
         description="stac-api landing page",
         stac_extensions=[
@@ -217,6 +218,7 @@ def test_api_landing_page():
 
 def test_api_landing_page_is_catalog():
     landing_page = LandingPage(
+        type="Catalog",
         id="test-landing-page",
         description="stac-api landing page",
         stac_extensions=[
