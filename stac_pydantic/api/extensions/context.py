cf3c89cfbb97bdf08aaf9018b2baf6c01392fc04
Thomas Maschler <thomas.maschler@planet.com>
2023-09-26 17:37:44 -0400

    fix context and mark as deprecated

--- stac_pydantic/api/extensions/context.py
+++ stac_pydantic/api/extensions/context.py
@@ -2,20 +2,34 @@ from typing import Any, Dict, Optional
 
 from pydantic import BaseModel, model_validator
 
+CONTEXT_VERSION = "v1.0.0-rc.2"
+
 
 class ContextExtension(BaseModel):
     """
-    https://github.com/radiantearth/stac-api-spec/tree/master/extensions/context#context-extension-specification
+    https://github.com/stac-api-extensions/context/tree/v1.0.0-rc.2#context-object
     """
 
     returned: int
     limit: Optional[int] = None
     matched: Optional[int] = None
 
-    # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-validators for more information.
     @model_validator(mode="before")
-    def validate_limit(cls, values: Dict[str, Any]) -> None:
+    def validate_limit(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        from warnings import warn
+
+        warn(
+            "STAC API - Context Extension is deprecated. "
+            "Please see https://github.com/stac-api-extensions/context for details.",
+            DeprecationWarning,
+        )
+
         if values["limit"] and values["returned"] > values["limit"]:
             raise ValueError(
                 "Number of returned items must be less than or equal to the limit"
             )
+        if values["matched"] and values["returned"] > values["matched"]:
+            raise ValueError(
+                "Number of returned items must be less than or equal to the matched items"
+            )
+        return values
